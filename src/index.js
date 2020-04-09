668fc027c841c7d9b0b9454419e1cf273343c9b2
ilia <ilyakut@gmail.com>
2020-04-09 17:47:35 +0300

    added list

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import AppHeader from "./components/AppHeader";
+import TodoList from "./components/TodoList";
+import SearchPanel from "./components/SearchPanel";
+
+const App = () => {
+
+  const todoData = [
+    {label: 'Drink Coffee', important: false, id: 1},
+    {label: 'Make Awesome App', important: true, id: 2},
+    {label: 'Have a lunch', important: false, id: 3}
+  ];
+
+  return (
+    <div>
+      <AppHeader/>
+      <SearchPanel/>
+      <TodoList todos={todoData}/>
+    </div>
+  );
+};
+
+ReactDOM.render(<App/>, document.getElementById('root'));
