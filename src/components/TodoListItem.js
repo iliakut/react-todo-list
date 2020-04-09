be94a3942487a9290b1e95059badc6c3651649a3
ilia <ilyakut@gmail.com>
2020-04-09 18:30:33 +0300

    TodoListItem css

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,12 +1,30 @@
 import React from "react";
+import "./TodoListItem.css"
 
 const TodoListItem = ({label, important = false}) => {
-  const textStyle = {
-    color: important ? 'tomato' : 'black'
+  const style = {
+    color: important ? 'steelblue' : 'black',
+    fontWeight: important ? 'bold' : 'normal'
   };
 
   return (
-    <span style={textStyle}>{label}</span>
+    <span className="todo-list-item">
+      <span
+        className="todo-list-item-label"
+        style={style}>
+        {label}
+      </span>
+
+      <button type="button"
+              className="btn btn-outline-success btn-sm float-right">
+        <i className="fa fa-exclamation"/>
+      </button>
+
+      <button type="button"
+              className="btn btn-outline-danger btn-sm float-right">
+        <i className="fa fa-trash-o"/>
+      </button>
+    </span>
   );
 };
 
