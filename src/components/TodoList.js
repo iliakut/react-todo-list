055c81de2606f54e54cd225a69d63413ab15db9f
ilia <ilyakut@gmail.com>
2020-04-09 17:46:58 +0300

    added TodoList.js component

---
+++ src/components/TodoList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import TodoListItem from "./TodoListItem";
+
+const TodoList = ({todos}) => {
+  const elements = todos.map(item => {
+
+    const { id, ...itemProps } = item;
+
+    return (
+      <li key={id}>
+        <TodoListItem
+          {...itemProps}
+        />
+      </li>
+    );
+  });
+
+  return (
+    <ul>
+      {elements}
+    </ul>
+  );
+};
+
+export default TodoList;
+
