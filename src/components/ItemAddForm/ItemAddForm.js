0b4e1eef008fecd89a085df14b844ebc3b31be33
ilia <ilyakut@gmail.com>
2020-04-12 16:38:48 +0300

    form to add todoItems

--- src/components/ItemAddForm/ItemAddForm.js
+++ src/components/ItemAddForm/ItemAddForm.js
@@ -3,18 +3,28 @@ import "./ItemAddForm.css";
 
 export default class ItemAddForm extends Component {
 
+  onLabelChange = () => {
+
+  };
+
   render() {
     const {onAddItem} = this.props;
 
     return (
-      <div>
+      <form className="item-add-form d-flex">
+        <input
+          type="text"
+          className="form-control"
+          onChange={this.onLabelChange}
+          placeholder="New ToDo item"
+        />
         <button
           type="button"
           className="btn btn-secondary"
           onClick={() => onAddItem('test')}
         >Add item
         </button>
-      </div>
+      </form>
     )
   }
 }
