e8b4c333e7602241573c5168513710b80a62c72d
ilia <ilyakut@gmail.com>
2020-04-16 19:41:43 +0300

    added SearchPanel functionality

--- src/components/SearchPanel/SearchPanel.js
+++ src/components/SearchPanel/SearchPanel.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./SearchPanel.css"
 
-const SearchPanel = () => {
+const SearchPanel = ({searchText, onSearch}) => {
   return (
     <input type="text"
+           name="search-input"
+           value={searchText}
            className="form-control search-input"
+           onChange={(e) => onSearch(e.target.value)}
            placeholder="type to search"/>
   );
 };
